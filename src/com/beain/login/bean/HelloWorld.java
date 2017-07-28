c0e180aeab6d62e9eafc817486a846e851bf2a78
Jayson <2123001816@qq.com>
2017-07-28 23:29:13 +0800

    Jayson修改HelloWorld,增加求和的方法

--- src/com/beain/login/bean/HelloWorld.java
+++ src/com/beain/login/bean/HelloWorld.java
@@ -2,7 +2,19 @@ package com.beain.login.bean;
 
 public class HelloWorld {
 	
+	int a;
+	int b;
+	
+	public static int getSum(int a, int b){
+		return a + b;
+	}
+	
 	public static void main(String[] args){
 		System.out.println("HelloWorld !");
+		
+		int c = getSum(5, 10);
+		System.out.println(c);
 	}
+	
+	
 }
