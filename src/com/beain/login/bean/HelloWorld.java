119b667216e4a2981e167b247382c81b8d68a4f5
Jayson <2123001816@qq.com>
2017-07-28 23:52:03 +0800

    Jayson第二次修改HelloWorld类,新增求积的方法

--- src/com/beain/login/bean/HelloWorld.java
+++ src/com/beain/login/bean/HelloWorld.java
@@ -1,20 +1,26 @@
 package com.beain.login.bean;
 
 public class HelloWorld {
-	
+
 	int a;
 	int b;
-	
-	public static int getSum(int a, int b){
+
+	public static int getSum(int a, int b) {
 		return a + b;
 	}
-	
-	public static void main(String[] args){
+
+	public static int getJi(int a, int b) {
+		return a * b;
+	}
+
+	public static void main(String[] args) {
 		System.out.println("HelloWorld !");
-		
+
 		int c = getSum(5, 10);
 		System.out.println(c);
+		
+		int d = getJi(8, 9);
+		System.out.println(d);
 	}
-	
-	
+
 }
